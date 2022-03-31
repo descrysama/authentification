48fc36594360c395b455cbbaabd7e8e99916a391
DescrySama <pierrehugot35@gmail.com>
2022-03-31 14:58:28 +0200

    commited

--- resources/views/dashboard/attack.blade.php
+++ resources/views/dashboard/attack.blade.php
@@ -8,19 +8,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden flex justify-center shadow-sm sm:rounded-lg">
                 <form class="flex flex-col" action="" method="POST">
-                    <label for="ip">Ip adress :</label>
+                    <label class="m-2" for="ip">Ip address  :</label>
                     <input type="text" name="ip" id="ip" placeholder="78.193.102.103">
-                    <label for="ip">Port :</label>
+                    <label class="m-2" for="ip">Port :</label>
                     <input type="text" name="port" id="port" placeholder="80">
-                    <label for="length">Time :</label>
+                    <label class="m-2" for="length">Time :</label>
                     <input type="text" name="length" id="length" placeholder="120">
-                    <label for="method"> Method :</label>
+                    <label class="m-2" for="method"> Method :</label>
                                 <select name="method" id="method">
                                     @foreach($methods as $method)
                                     <option value="{{$method}}">{{$method}}</option>
                                     @endforeach
                                 </select>
-                    <input class="submitbutton" type="submit" value="Send">
+                    <input class="submitbutton" type="submit" value="Attack">
                 </form>
             </div>
         </div>
@@ -36,8 +36,8 @@
                             <th>Length</th>
                             <th>Port</th>
                             <th>Date</th>
-                            <th>Actions</th>
                             <th>State</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
