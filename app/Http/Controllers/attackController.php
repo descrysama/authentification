67e98232f2e264791713650874509de63c224371
DescrySama <pierrehugot35@gmail.com>
2022-03-31 14:53:40 +0200

    'self attacks only'

--- app/Http/Controllers/attackController.php
+++ app/Http/Controllers/attackController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\attack;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class attackController extends Controller
 {
     public function index()
     {
         $methods = ['NTP', 'UDP', 'SNMP', 'SSDP'];
-        $attacks = attack::all();
+        $attacks = attack::all()->where('sender_id', Auth::user()->id);
         return view('dashboard.attack', ['methods' => $methods], ['attacks' => $attacks]);
     }
 }
